dd6c268b37c74d60b48a4dfbfa5a7b0da8af8c78
Leandro Lanza <tucholanza@gmail.com>
2022-12-26 21:05:24 -0300

    PRIMERA CARGA

---
+++ data.js
@@ -0,0 +1,506 @@
+listaDeProductos = [
+    {
+        id: "1",
+        titulo: "Casco Bell Avenue Mips",
+        imagen: "./imagenes/cascos/bell/casco bell avenue mips.jpg",
+        categoria: {
+            id: "Cascos",
+            nombre: "Cascos",
+        },
+        precio: "$15500",
+
+    },
+    {
+        id: "2",
+        titulo: "Casco Bell Strat",
+        imagen: "./imagenes/cascos/bell/casco bell strat.jpg",
+        categoria: {
+            id: "Cascos",
+            nombre: "Cascos",
+        },
+        precio: "$24000",
+
+    }, {
+        id: "3",
+        titulo: "Casco Bell Trace Mips",
+        imagen: "./imagenes/cascos/bell/casco bell trace mips.jpg",
+        categoria: {
+            id: "Cascos",
+            nombre: "Cascos",
+        },
+        precio: "$22000",
+
+    }, {
+        id: "4",
+        titulo: "Casco Giro Aether Spherical",
+        imagen: "./imagenes/cascos/giro/casco giro aether spherical.jpg",
+        categoria: {
+            id: "Cascos",
+            nombre: "Cascos",
+        },
+        precio: "$24300",
+
+    }, {
+        id: "5",
+        titulo: "Absolute Black Oval Dorado",
+        imagen: "./imagenes/absolute black/PLATOS/absolute black Oval dorado.jpg",
+        categoria: {
+            id: "transmision",
+            nombre: "Platos",
+        },
+        precio: "$13000",
+
+    }, {
+        id: "6",
+        titulo: "Absolute Black Oval Naranja",
+        imagen: "./imagenes/absolute black/PLATOS/absolute black Oval Naranja .jpg",
+        categoria: {
+            id: "transmision",
+            nombre: "Platos",
+        },
+        precio: "$13000",
+
+    }, {
+        id: "7",
+        titulo: "Absolute Black Oval Negro",
+        imagen: "./imagenes/absolute black/PLATOS/absolute black Oval negro.jpg",
+        categoria: {
+            id: "transmision",
+            nombre: "Platos",
+        },
+        precio: "$13000",
+    }, {
+        id: "8",
+        titulo: "Absolute Black Oval Purpura",
+        imagen: "./imagenes/absolute black/PLATOS/absolute black Oval Púrpura.jpg",
+        categoria: {
+            id: "transmision",
+            nombre: "Platos",
+        },
+        precio: "$13000",
+    }, {
+        id: "9",
+        titulo: "Absolute Black Oval Rojo",
+        imagen: "./imagenes/absolute black/PLATOS/absolute black Oval rojo.jpg",
+        categoria: {
+            id: "transmision",
+            nombre: "Platos",
+        },
+        precio: "$13000",
+    }, {
+        id: "10",
+        titulo: "Absolute Black Oval Titanium",
+        imagen: "./imagenes/absolute black/PLATOS/absolute black Oval Titanium.jpg",
+        categoria: {
+            id: "transmision",
+            nombre: "Platos",
+        },
+        precio: "$13000",
+    }, {
+        id: "11",
+        titulo: "Absolute Black Rendodo Negro",
+        imagen: "./imagenes/absolute black/PLATOS/absolute black redondo negro.jpg",
+        categoria: {
+            id: "transmision",
+            nombre: "Platos",
+        },
+        precio: "$13000",
+    }, {
+        id: "12",
+        titulo: "Roldana Absolute Black",
+        imagen: "imagenes/absolute black/ROLDANAS/absoluteBLACK-HOLLOWcage-SRAM-AXS-aero-hollow-ceramic-derailleur-pulley-upgrade_colors.jpg",
+        categoria: {
+            id: "transmision",
+            nombre: "Roldanas",
+        },
+        precio: "$8200",
+    }, {
+        id: "13",
+        titulo: "HOLLOWcage Cargbon Ceramic Oversized Derailleur Pulley Cage black",
+        imagen: "./imagenes/absolute black/ROLDANAS/HOLLOWcage Cargbon Ceramic Oversized Derailleur Pulley Cage black.jpg",
+        categoria: {
+            id: "transmision",
+            nombre: "Roldanas",
+        },
+        precio: "$8200",
+    }, {
+        id: "14",
+        titulo: "HOLLOWcage Cargbon Ceramic Oversized Derailleur Pulley Cage red",
+        imagen: "./imagenes/absolute black/ROLDANAS/HOLLOWcage Cargbon Ceramic Oversized Derailleur Pulley Cage red.jpg",
+        categoria: {
+            id: "transmision",
+            nombre: "Roldanas",
+        },
+        precio: "$8200",
+    }, {
+        id: "15",
+        titulo: "HOLLOWcage Cargbon Ceramic Oversized Derailleur Pulley Cage Yellow",
+        imagen: "./imagenes/absolute black/ROLDANAS/HOLLOWcage Cargbon Ceramic Oversized Derailleur Pulley Cage Yellow.jpg",
+        categoria: {
+            id: "transmision",
+            nombre: "Roldanas",
+        },
+        precio: "$8200",
+    }, {
+        id: "16",
+        titulo: "Finish Line Chill Zone",
+        imagen: "./imagenes/finish line/aerosol chill zone.jpg",
+        categoria: {
+            id: "lubricantes",
+            nombre: "lubricantes",
+        },
+        precio: "$3200",
+    }, {
+        id: "17",
+        titulo: "Finish Line Gel fiber Grip",
+        imagen: "imagenes/finish line/gel fiber grip.jpg",
+        categoria: {
+            id: "lubricantes",
+            nombre: "lubricantes",
+        },
+        precio: "$3200",
+    }, {
+        id: "18",
+        titulo: "Finish Line Grasa Ceramica",
+        imagen: "./imagenes/finish line/grasa ceramica.jpg",
+        categoria: {
+            id: "lubricantes",
+            nombre: "lubricantes",
+        },
+        precio: "$3200",
+    }, {
+        id: "19",
+        titulo: "Finish Line Grasa Premium",
+        imagen: "./imagenes/finish line/grasa premium.jpg",
+        categoria: {
+            id: "lubricantes",
+            nombre: "lubricantes",
+        },
+        precio: "$3200",
+    }, {
+        id: "20",
+        titulo: "Finish Line Lubricante Humedo",
+        imagen: "./imagenes/finish line/lubircante humedo.jpg",
+        categoria: {
+            id: "lubricantes",
+            nombre: "lubricantes",
+        },
+        precio: "$3200",
+    }, {
+        id: "21",
+        titulo: "Squit 15ml",
+        imagen: "./imagenes/squirt/squirt-15ml.jpg",
+        categoria: {
+            id: "lubricantes",
+            nombre: "lubricantes",
+        },
+        precio: "$3200",
+    }, horquillas
+        id: "22",
+        titulo: "Squirt 120ml",
+        imagen: "./imagenes/squirt/squirt-12/ml.jpg",
+        categoria: {
+            id: "lubricantes",
+            nombre: "lubricantes",
+        },
+        precio: "$3200",
+    }, {
+        id: "23",
+        titulo: "Horquilla Rockshox Judy",
+        imagen: "./imagenes/horquillas/Horquilla Rockshox Judy.jpg",
+        categoria: {
+            id: "horquillas",
+            nombre: "horquillas",
+        },
+        precio: "$3200",
+    }, {
+        id: "24",
+        titulo: "Horquilla Rockshox Reba RL 29 SoloAir 100mm 15mm Boost Tapered OneLoc",
+        imagen: "./imagenes/horquillas/Horquilla Rockshox Reba RL 29 SoloAir 100mm 15mm Boost Tapered OneLoc.jpg",
+        categoria: {
+            id: "horquillas",
+            nombre: "horquillas",
+        },
+        precio: "$3200",
+    }, {
+        id: "25",
+        titulo: "Horquilla Rockshox Recon Silver RL 29 Solo Air",
+        imagen: "./imagenes/horquillas/Horquilla Rockshox Recon Silver RL 29 Solo Air.jpg",
+        categoria: {
+            id: "horquillas",
+            nombre: "horquillas",
+        },
+        precio: "$3200",
+    }, {
+        id: "26",
+        titulo: "Horquilla Rockshox SID SL Select 29 DebonAir 100mm 15mm Boost Tapered TwistLoc",
+        imagen: "./imagenes/horquillas/Horquilla Rockshox SID SL Select 29 DebonAir 100mm 15mm Boost Tapered TwistLoc.jpg",
+        categoria: {
+            id: "horquillas",
+            nombre: "horquillas",
+        },
+        precio: "$3200",
+    }, {
+        id: "27",
+        titulo: "Flores AcuarelHorquilla Rockshox SID SL Ultimate 29 DebonAir 100mm 15mm Boost Tapered TwiLoc",
+        imagen: "./imagenes/horquillas/Horquilla Rockshox SID SL Ultimate 29 DebonAir 100mm 15mm Boost Tapered TwiLoc..jpg",
+        categoria: {
+            id: "horquillas",
+            nombre: "horquillas",
+        },
+        precio: "$3200",
+    }, {
+        id: "28",
+        titulo: "SID SL Ultimate",
+        imagen: "./imagenes/horquillas/SID SL Ultimate.png",
+        categoria: {
+            id: "horquillas",
+            nombre: "horquillas",
+        },
+        precio: "$3200",
+    }, {
+        id: "29",
+        titulo: "Cube Reaction C62 Race 2021",
+        imagen: "./imagenes/linea cube/Bicicleta Cube Reaction C62 Race 2021.jpg",
+        categoria: {
+            id: "bicicletas",
+            nombre: "bicicletas",
+        },
+        precio: "$3200",
+    },{
+        id: "30",
+        titulo: "Cube Acid 2021",
+        imagen: "./imagenes/linea cube/cube acid 2021.jpg",
+        categoria: {
+            id: "bicicletas",
+            nombre: "bicicletas",
+        },
+        precio: "$3200",
+    },{
+        id: "31",
+        titulo: "Cube Aim 2021",
+        imagen: "./imagenes/linea cube/cube aim 2021.jpg",
+        categoria: {
+            id: "bicicletas",
+            nombre: "bicicletas",
+        },
+        precio: "$3200",
+    },{
+        id: "32",
+        titulo: "Cube Analog 2021",
+        imagen: "./imagenes/linea cube/cube analog 2021.jpg",
+        categoria: {
+            id: "bicicletas",
+            nombre: "bicicletas",
+        },
+        precio: "$3200",
+    },{
+        id: "33",
+        titulo: "Cube Atention 2021",
+        imagen: "./imagenes/linea cube/cube atention 2021.jpg",
+        categoria: {
+            id: "bicicletas",
+            nombre: "bicicletas",
+        },
+        precio: "$3200",
+    },{
+        id: "34",
+        titulo: "Cube Attain GTC SL 28 2021",
+        imagen: "./imagenes/linea cube/Cube Attain GTC SL 28 2021.jpg",
+        categoria: {
+            id: "bicicletas",
+            nombre: "bicicletas",
+        },
+        precio: "$3200",
+    },{
+        id: "35",
+        titulo: "Cube Elite C68X Pro",
+        imagen: "./imagenes/linea cube/cube elite c68x pro.jpg",
+        categoria: {
+            id: "bicicletas",
+            nombre: "bicicletas",
+        },
+        precio: "$3200",
+    },{
+        id: "36",
+        titulo: "cube elite c68x",
+        imagen: "./imagenes/linea cube/cube elite c68x.jpg",
+        categoria: {
+            id: "bicicletas",
+            nombre: "bicicletas",
+        },
+        precio: "$3200",
+    },{
+        id: "37",
+        titulo: "cube reaction c62",
+        imagen: "./imagenes/linea cube/cube reaction c62.jpg",
+        categoria: {
+            id: "bicicletas",
+            nombre: "bicicletas",
+        },
+        precio: "$3200",
+    },{
+        id: "38",
+        titulo: "Rotor Elvedes",
+        imagen: "./imagenes/rotores Elvedes/rotor ELVEDES.jpg",
+        categoria: {
+            id: "varios",
+            nombre: "VArios",
+        },
+        precio: "$3200",
+    },{
+        id: "39",
+        titulo: "Shimano Deore 4100",
+        imagen: "./imagenes/deore 4100.jpg",
+        categoria: {
+            id: "transmision",
+            nombre: "shimano",
+        },
+        precio: "$3200",
+    },{
+        id: "40",
+        titulo: "Shimano Deore 5400",
+        imagen: "./imagenes/shimano/deore 5400.jpg",
+        categoria: {
+            id: "transmision",
+            nombre: "shimano",
+        },
+        precio: "$3200",
+    },{
+        id: "41",
+        titulo: "Shimano Deore 6100",
+        imagen: "./imagenes/shimano/deore m6100.jpg",
+        categoria: {
+            id: "transmision",
+            nombre: "shimano",
+        },
+        precio: "$3200",
+    },{
+        id: "42",
+        titulo: "Shimano Deore XT",
+        imagen: "./imagenes/shimano/deore xt.jpg",
+        categoria: {
+            id: "transmision",
+            nombre: "shimano",
+        },
+        precio: "$3200",
+    },{
+        id: "43",
+        titulo: "Grupa Sram GX Eagle 1x12v",
+        imagen: "./imagenes/sram/Grupo MTB SRAM GX Eagle 1x12v 10-52d Boost 175mm DUB 32d Lunar.jpg",
+        categoria: {
+            id: "transmision",
+            nombre: "sram",
+        },
+        precio: "$3200",
+    },{
+        id: "44",
+        titulo: "Piñon SRAM 12v MTB XG-1295 10-50d XD Black (X01 Eagle)",
+        imagen: "./imagenes/sram/Piñon SRAM 12v MTB XG-1295 10-50d XD Black (X01 Eagle).jpeg",
+        categoria: {
+            id: "transmision",
+            nombre: "sram",
+        },
+        precio: "$3200",
+    },{
+        id: "45",
+        titulo: "Piñon SRAM 12v MTB XG-1295 10-50d XD Polar Grey (X01 Eagle)",
+        imagen: "./imagenes/sram/Piñon SRAM 12v MTB XG-1295 10-50d XD Polar Grey (X01 Eagle).jpeg",
+        categoria: {
+            id: "transmision",
+            nombre: "sram",
+        },
+        precio: "$3200",
+    }{
+        id: "46",
+        titulo: "Piñon SRAM 12v MTB XG-1299 10-50d XD Rainbown (XX1 Eagle)",
+        imagen: "./imagenes/sram/Piñon SRAM 12v MTB XG-1299 10-50d XD Rainbown (XX1 Eagle).jpg",
+        categoria: {
+            id: "transmision",
+            nombre: "sram",
+        },
+        precio: "$3200",
+    },{
+        id: "47",
+        titulo: "Frenos Shimano MT200 ",
+        imagen: "./imagenes/shimano/shimano mt200.jpg",
+        categoria: {
+            id: "frenos",
+            nombre: "shimano",
+        },
+        precio: "$3200",
+    },{
+        id: "48",
+        titulo: "Frenos Shimano MT6100",
+        imagen: "./img/foto29.jpeg",
+        categoria: {
+            id: "frenos",
+            nombre: "shimano",
+        },
+        precio: "$3200",
+    }/*,{
+        id: "49",
+        titulo: "Frenos Sram",
+        imagen: "./imagenes/sram/frenos sram sb/foto29.jpeg",
+        categoria: {
+            id: "frenos",
+            nombre: "sram",
+        },
+        precio: "$3200",
+    },{
+        id: "50",
+        titulo: "Particiones",
+        imagen: "./img/foto29.jpeg",
+        categoria: {
+            id: "cuadros",
+            nombre: "Cuadros",
+        },
+        precio: "$3200",
+    },{
+        id: "51",
+        titulo: "Particiones",
+        imagen: "./img/foto29.jpeg",
+        categoria: {
+            id: "cuadros",
+            nombre: "Cuadros",
+        },
+        precio: "$3200",
+    },{
+        id: "52",
+        titulo: "Particiones",
+        imagen: "./img/foto29.jpeg",
+        categoria: {
+            id: "cuadros",
+            nombre: "Cuadros",
+        },
+        precio: "$3200",
+    },{
+        id: "53",
+        titulo: "Particiones",
+        imagen: "./img/foto29.jpeg",
+        categoria: {
+            id: "cuadros",
+            nombre: "Cuadros",
+        },
+        precio: "$3200",
+    },{
+        id: "54",
+        titulo: "Particiones",
+        imagen: "./img/foto29.jpeg",
+        categoria: {
+            id: "cuadros",
+            nombre: "Cuadros",
+        },
+        precio: "$3200",
+    },{
+        id: "55",
+        titulo: "Particiones",
+        imagen: "./img/foto29.jpeg",
+        categoria: {
+            id: "cuadros",
+            nombre: "Cuadros",
+        },
+        precio: "$3200",
+    }*/
+
+]
