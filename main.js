d5925dd94dae082bec4f80a0b7a93ea36630ee37
Leandro <tucholanza@gmail.com>
2023-01-02 15:02:17 -0300

    detalles

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const copyMenu = () => {
-    let dptCategory = document.querySelector('.dpt-cat')
-    let dptPlace = document.querySelector('.departments')
+    var dptCategory = document.querySelector('.dpt-cat')
+    var dptPlace = document.querySelector('.departments')
     dptPlace.innerHTML = dptCategory.innerHTML;
-    let mainNav = document.querySelector('.header-nav nav')
-    let navPlace = document.querySelector('.off-canvas nav')
+    var mainNav = document.querySelector('.header-nav nav')
+    var navPlace = document.querySelector('.off-canvas nav')
     navPlace.innerHTML = mainNav.innerHTML;
-    let topNav = document.querySelector('.header-top')
-    let topPlace = document.querySelector('.off-canvas .thetop-nav')
+    let topNav = document.querySelector('.header-top .wrapper')
+    var topPlace = document.querySelector('.off-canvas .thetop-nav')
     topPlace.innerHTML = topNav.innerHTML;
 
 }
@@ -25,13 +25,14 @@ closeButton.addEventListener('click', function(){
     addClass.classList.remove('showmenu')
 });
 
-const subMenu = document.querySelectorAll('.has-child .icon-small');
-subMenu.forEach((menu) => menu.addEventListener('click', toggle));
+const submenu = document.querySelectorAll('.has-child');
+submenu.forEach((menu) => menu.addEventListener('click', toggle));
 
-const toggle = (e) => {
+function toggle (e){
     e.preventDefault();
-    subMenu.forEach((item) => item != this ? item.closest('.has-child').classList.remove('expand'): null);
-    if(this.closet('.has-child').classList!='expand');
-    this.closet('.has-child').classList.toggle('expand')
+    submenu.forEach((item) => item != this ? item.closest('.has-child').classList.remove('expand'): null);
+    if(this.closest('.has-child').classList != 'expand');
+    this.closest('.has-child').classList.toggle('expand')
 }
+
 
