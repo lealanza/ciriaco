d15e6193988a86a18d22254b651547d2082cf9d1
Leandro <tucholanza@gmail.com>
2023-01-10 15:59:06 -0300

    buscador a medias

--- main.js
+++ main.js
@@ -1,41 +1,21 @@
 const divProductos = document.getElementById('div-container-productos');
 const btnCategory = document.querySelectorAll('.btn-categoria');
 const changeTitle = document.getElementById('titulo-principal');
+const changeTitleAside = document.getElementById('titulo-aside');
 let btnAddProduct = document.querySelectorAll(".btd-add-product");
 const numberCart = document.querySelector('#numerito')
-const searchInput = document.getElementById('#search-input')
+const searchInput = document.getElementById("#search-input")
 const formu = document.getElementById("formulario");
 const agregarBtn = document.getElementById("idButton");
+const divProductosDetails = document.getElementById("#div-container-productos-details")
 
-const findProducto = (value) => listaDeProductos.find(producto => producto.marca == value)
-
-const showEmptyError = () => {
-    divProductos.innerHTML = `
-    <div class="cardError">
-        <h2 class="error-title">Articulo no encontrado</h2>
-    </div>`
-    divProductos.append(div);
-}
-
-//slider
-const swiper = new Swiper('.swiper', {
-    loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-       
-      },
-  });
 
 cargarProductos = (productosElegidos) => {
 
     divProductos.innerHTML = "";
     productosElegidos.forEach((producto) => {
         const div = document.createElement('div');
-        /* div.classList.add('producto');*/
+
         div.innerHTML = `<div class="producto">
         <img class="prodcuto-imagen" src="${producto.imagen}">
         <div class="productos-detalles">
@@ -49,78 +29,42 @@ cargarProductos = (productosElegidos) => {
     </div> 
         `;
         divProductos.append(div);
-     
+
     });
-   refreshBtnAdd();
-   
+    refreshBtnAdd();
+
 };
 
-const renderResult = (producto) => {
-    if(!producto){
-        divProductos.innerHTML = `
-        <div class="productos-detalles">
-        <h3 class="producto-titulo">Articulo no encontrado</h3>
-        </div>`
-        divProductos.append(div);
-    }else{
-        divProductos.innerHTML =`<div class="producto">
-        <img class="prodcuto-imagen" src="${producto.imagen}">
-        <div class="productos-detalles">
-            <h3 class="producto-titulo">${producto.titulo}</h3>
-            <p class="producto-precio">${producto.precio}</p>
-            <a href="detalles.html" class="btd-details-product btd-product" id="${producto.id}">Detalles</a>
-            <a class="btd-add-product" id="${producto.id}">Agregar</a>
-        </div>
-        
-        </div>
-    </div> `;
-    divProductos.append(div);
-    }
-}
-const refreshBtnAdd = () =>{
+const refreshBtnAdd = () => {
     btnAddProduct = document.querySelectorAll('.btd-add-product');
     btnAddProduct.forEach(boton => {
-        boton.addEventListener("click", agregarCarrito)});
+        boton.addEventListener("click", agregarCarrito)
+    });
 }
-const searchProducto = (e) =>{
-    e.preventDefault();
-    const valorBuscador = searchInput.value;
-    if(!valorBuscador){
-        showEmptyError();
-        return;
-    }
-    const searchedProduct = findProducto(marca(valorBuscador))
-   
-        cargarProductos(searchedProduct);
-        valorBuscador.value="";
-        formu.reset();
-}
+const productosCarrito = [];
 
-
-const productosCarrito= [];
-
-const agregarCarrito = (e) =>{
+const agregarCarrito = (e) => {
     const idBtn = e.currentTarget.id;
-    const productoAdd = listaDeProductos.find(producto => producto.id==idBtn);
-    
-    if(productosCarrito.some(producto => producto.id==idBtn)){
-        const index = productosCarrito.findIndex(producto => producto.id==idBtn)
-        
+    const productoAdd = listaDeProductos.find(producto => producto.id == idBtn);
+
+    if (productosCarrito.some(producto => producto.id == idBtn)) {
+        const index = productosCarrito.findIndex(producto => producto.id == idBtn)
+
         productosCarrito[index].cantidad++;
         listaDeProductos.cantidad--;
-        
+
     } else {
         productoAdd.cantidad;
         productosCarrito.push(productoAdd);
-        productoAdd.cantidad-1;
+        productoAdd.cantidad - 1;
     }
     refreshNumberCart();
     localStorage.setItem("productos-carrito", JSON.stringify(productosCarrito));
 }
-    
-const refreshNumberCart =()=>{
-    let numerito = productosCarrito.reduce((acc, producto) => acc + producto.cantidad,0)
-    numberCart.innerHTML=numerito;
+
+const refreshNumberCart = () => {
+    let numerito = productosCarrito.reduce((acc, producto) => acc + producto.cantidad, 0)
+    numberCart.innerHTML = numerito;
 }
 
 
@@ -132,8 +76,9 @@ btnCategory.forEach(boton => {
         btnCategory.forEach(boton => boton.classList.remove("active"));
         e.currentTarget.classList.add("active");
         if (e.currentTarget.id != "todos") {
-            const productosCategorias = listaDeProductos.find(producto => producto.categoria.id === e.currentTarget.id )
-            changeTitle.innerHTML= productosCategorias.categoria.id;
+            const productosCategorias = listaDeProductos.find(producto => producto.categoria.id === e.currentTarget.id)
+            changeTitle.innerHTML = productosCategorias.categoria.id;
+            changeTitleAside.innerHTML = productosCategorias.categoria.id;
             const productoBtn = listaDeProductos.filter(producto => producto.categoria.id === e.currentTarget.id)
             cargarProductos(productoBtn)
         } else {
@@ -143,8 +88,13 @@ btnCategory.forEach(boton => {
     })
 });
 
-const init = ()=>{
-    formu.addEventListener("submit", searchProducto);
-}
 
-init();
+document.addEventListener("keyup", e => {
+    if (e.target.matches("#search-input")) {
+        document.querySelectorAll(".producto").forEach(producto => {
+            producto.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+                ? producto.classList.remove("dissable")
+                : producto.classList.add("dissable")
+            })
+    }
+});
